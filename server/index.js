a8551e898ba905807ffa5372d3454262bfb0e8fb
Nastya Stashevskaya <stashevskayaaa@gmail.com>
2021-04-01 17:25:41 +0300

    feat: add files directory

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+
 app.get('/', (req, res) => {
   res.send('Hello from cyberpizza!')
 })
+
+app.use('/files', express.static('./files'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
