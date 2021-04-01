8ebb59d8339a14502d777d6188f001efb4dcbd38
Nastya Stashevskaya <stashevskayaaa@gmail.com>
2021-04-01 17:11:29 +0300

    feat: add webpack config

---
+++ client/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { hot } from 'react-hot-loader/root'
+
+const App = () => {
+    return <div>Hello from cyberpizza React!</div>
+}
+
+export default hot(App)
--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello from cyberpizza!')
+    res.send('Hello from cyberpizza!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
