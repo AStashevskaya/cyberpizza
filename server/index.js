8d604bad58c19bbf6d4fa28943cc12bde6e5fcd3
Nastya Stashevskaya <stashevskayaaa@gmail.com>
2021-04-01 18:05:52 +0300

    fix: fix server

--- server/index.js
+++ server/index.js
@@ -2,11 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello from cyberpizza!')
-})
-
-app.use(express.static('./dist'))
+app.use(express.static('dist'))
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
