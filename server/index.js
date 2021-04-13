09116e76f2f6fb32b42e6e57790fd13a3463b962
Nastya Stashevskaya <stashevskayaaa@gmail.com>
2021-04-13 13:28:14 +0300

    fix: deploy 10

--- server/index.js
+++ server/index.js
@@ -24,7 +24,9 @@ async function start() {
     app.use(bodyParser.json({ extended: true }))
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(express.static('dist'))
-    app.get('*', (req, res) => res.sendFile(path.resolve('dist', 'index.html')))
+    app.get("/*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../dist/index.html"));
+    });
     app.use('/files', express.static(path.join(__dirname, './files')))
     app.use(productsRouts)
     app.listen(port, 'localhost', () => {
