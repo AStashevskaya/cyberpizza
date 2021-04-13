0c92fc3145b41c570ff45797c45f0ae5abb73cea
Nastya Stashevskaya <stashevskayaaa@gmail.com>
2021-04-13 12:48:50 +0300

    fix: deploy 6

--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import { hot } from 'react-hot-loader/root'
 
 import CatalogPage from './pages/catalog'
@@ -7,7 +7,7 @@ import CatalogPage from './pages/catalog'
 const App = () => {
   return (
     <div>
-      <Router>
+      <Router hashType="noslash">
         <Switch>
           <Route exact path="/" component={CatalogPage} />
         </Switch>
