f724952920dd82e8c673fa7f42d37eec5c38998e
Nastya Stashevskaya <stashevskayaaa@gmail.com>
2021-04-16 19:03:39 +0300

    add empty image

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const path = require('path')
 
 
 app.get('/', (req, res) => {
   res.send('Hello from cyberpizza!')
 })
 
-app.use('/files', express.static('./files'))
-
+app.use('/files', express.static(path.join(__dirname, './files')))
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
